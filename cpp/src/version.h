cf4dc0be6a5d33560c641b410325beecc92aa495
Siddhesh Dhinge <saddy97531@gmail.com>
2026-05-24 00:10:50 +0530

    add version file

---
+++ cpp/src/version.h
@@ -0,0 +1,20 @@
+#ifndef HLLPLUSPLUS_H
+#define HLLPLUSPLUS_H
+
+namespace bareboneshll::version {
+
+constexpr int VERSION_MAJOR {0};
+constexpr int VERSION_MINOR {5};
+constexpr int VERSION_PATCH {0};
+
+// Optional build metadata
+constexpr int VERSION_TWEAK {20260524};
+
+constexpr auto VERSION_STR = "0.5.0";
+constexpr auto VERSION_FULL_STR = "0.5.0.20260524";
+
+constexpr auto SOURCE_URL = "https://github.com/BareBonesLib/barebones-hll";
+
+} // namespace bareboneshll::version
+
+#endif HLLPLUSPLUS_H
